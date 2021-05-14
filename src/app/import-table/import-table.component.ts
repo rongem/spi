32fab95a41285ddccbdeca56444446767b4a4091
Michael Ronge <github@ronge.org>
2021-05-14 19:57:20 +0200

    capturing click events outside table and remove selection mark

--- src/app/import-table/import-table.component.ts
+++ src/app/import-table/import-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
 import { FrontendService } from '../lib/frontend.service';
 
 @Component({
@@ -7,6 +7,8 @@ import { FrontendService } from '../lib/frontend.service';
   styleUrls: ['./import-table.component.scss']
 })
 export class ImportTableComponent implements OnInit {
+  selectedLine = -1;
+  selectedCol = -1;
   get columns() {
     return this.fes.columns;
   }
@@ -15,13 +17,31 @@ export class ImportTableComponent implements OnInit {
     return this.columns.filter(c => c.internalName.toLowerCase() !== 'id');
   }
 
-  constructor(private fes: FrontendService) { }
+  @HostListener('document:click', ['$event'])
+  clickout(event: MouseEvent) {
+    if(this.eRef.nativeElement.contains(event.target)) {
+      this.setPasteIndex(-1, -1);
+    } else {
+      this.setPasteIndex(-1, -1);
+    }
+  }
+
+
+  constructor(private fes: FrontendService, private eRef: ElementRef) { }
 
   ngOnInit(): void {
   }
 
   onPaste(colIndex: number, event: ClipboardEvent) {
     console.log(colIndex, event);
+  }
+
+  setPasteIndex(line: number, col: number, event?: MouseEvent) {
+    this.selectedLine = line;
+    this.selectedCol = col;
+    if (event) {
+      event.cancelBubble = true;
+    }
   }
 
 }
